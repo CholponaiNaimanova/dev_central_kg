5c3bc85bec151529e54892e231d1c8ca6d3a9496
CholponaiNaimanova <naimanovacholpon@gmail.com>
2024-08-10 18:51:46 +0600

    login

--- src/components/ModalLogin/index.tsx
+++ src/components/ModalLogin/index.tsx
@@ -1,28 +1,115 @@
 import React from 'react';
+import axios from 'axios';
 
 interface LoginModalProps {
     show: boolean;
     handleClose: () => void;
 }
 
+const loginAPI = 'http://3.38.98.134/auth/login'
+const signupAPI = 'http://3.38.98.134/auth/signup'
+
+
 const ModalLogin: React.FC<LoginModalProps> = ({ show, handleClose }) => {
+
+    const [userName, setUserName] = React.useState<string>('')
+    const [password, setPassword] = React.useState<string>('')
+    const [consfirmPassword, setConsfirmPassword] = React.useState<string>('')
+    const [isLoginTab, setIsLoginTab] = React.useState<boolean>(true)
+
+    console.log('email', userName);
+    console.log('password', password);
+
+    const handleAuth = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!userName || !password) (
+            alert('Please fill all the fields')
+        )
+        if (!isLoginTab && password !== consfirmPassword) {
+            alert("Password do not match")
+        }
+        const authURL = isLoginTab ? loginAPI : signupAPI
+        try{
+            axios.post(authURL,{
+                userName,
+                password
+            })
+            .then(res => {
+                const {token, message} = res.data
+                if(res.data.success){
+                    console.log("data", res.data);
+                    alert(message)
+                } else{
+                    alert(message)
+                }
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+    
     return (
         <div className={`modal ${show ? 'show' : ''}`} onClick={handleClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
 
                 <button className="close" onClick={handleClose}>Закрыть</button>
 
-                <div className="modal-text">
-                    <h1>Войти через Telegram:</h1>
-                    <button className='btn'>Войти через Telegram</button>
-                </div>
+                <form className='login' onSubmit={handleAuth}>
+                    <h1>{isLoginTab ? 'Login' : 'Sign Up'}</h1>
 
-                <div className="image">
-                    <img src="https://devkg.com/js/img/not-found-jobs.f847c90.svg" alt="img" width={270}/>
-                </div>
+                    <input      
+                    onChange={(e) => setUserName(e.target.value)}
+                    value={userName}
+                     type="text" placeholder='email'/>
 
-                <h2>*Если в вашей стране заблокирован телеграм, используйте VPN</h2>
-                
+
+                    <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                     type="password" placeholder='password'/>
+
+{
+                    isLoginTab && (
+                    <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={consfirmPassword}
+                     type="password" placeholder='password'/>
+                    )}
+
+                    <button className='submitBtn' type='submit'>
+                        {isLoginTab ? 'Login' : 'Sign Up'}
+                    </button>
+
+                    {
+                        isLoginTab? (
+                            <div className="register">
+                        <p>Don't have an account?</p>
+                        <button className='signupLink'
+                        onClick={(e) => {
+                            e.preventDefault()
+                            setIsLoginTab(false)
+                        }}
+                        >Signup</button>
+                    </div>
+                        ): (
+                            <div className="register">
+                        <p>Already have an account?</p>
+                        <button className='signupLink'
+                        onClick={(e) => {
+                            e.preventDefault()
+                            setIsLoginTab(true)
+                        }}
+                        >Login</button>
+                    </div>
+                        )
+                    }
+
+                </form>
             </div>
 
 
